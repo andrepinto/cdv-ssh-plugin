bd1c89f8f2638454bf29149c3cff653f796f48de
Andre Pinto <andre40826@gmail.com>
2015-01-07 11:27:08 +0000

    scp refactoring

--- src/windows8/ScpHandler.js
+++ src/windows8/ScpHandler.js
@@ -1,56 +1,56 @@
- cordova.define("com.tlantic.plugins.ssh.ScpHandler", function(require, exports, module) {
+cordova.define("com.tlantic.plugins.ssh.ScpHandler", function(require, exports, module) {
 
-     exports.connect = function(win, fail, ip, username, password, port) {
-         console.log('Connect SSH...');
+    exports.connect = function(win, fail, ip, username, password, port) {
+        console.log('Connect SSH...');
 
-         try {
-             var instance = TlantSSH.TlanticSsh.instance;
-             instance.connect(ip, username, password, port);
-             if (instance.isConnect()) {
-                 win(instance);
-                 return;
-             } else {
-                 fail('NOT_CONNECTED');
-                 return;
-             }
+        try {
+            var instance = TlantSSH.TlanticSsh.instance;
+            instance.connect(ip, username, password, port);
+            if (instance.isConnect()) {
+                win(instance);
+                return;
+            } else {
+                fail('NOT_CONNECTED');
+                return;
+            }
 
-         } catch (e) {
-             fail(e);
-             return;
-         }
+        } catch (e) {
+            fail(e);
+            return;
+        }
 
-     };
+    };
 
-     exports.scp = function(win, fail, connection, file, path) {
-         try {
-             if (connection.isConnect()) {
-                 connection.scp("/tlanBiS/loadinfo/our_output/mrs/", "xmlfile.xml");
-                 win('FILED_SEND_WITH_SUCCESS');
-                 return;
-             } else {
-                 fail('NOT_CONNECTED');
-                 return;
-             }
+    exports.scp = function(win, fail, connection, file, path) {
+        try {
+            if (connection.isConnect()) {
+                connection.scp(path, file);
+                win('FILED_SEND_WITH_SUCCESS');
+                return;
+            } else {
+                fail('NOT_CONNECTED');
+                return;
+            }
 
-         } catch (e) {
-             fail(e);
-             return;
-         }
+        } catch (e) {
+            fail(e);
+            return;
+        }
 
-     };
+    };
 
-     exports.disconnect = function(win, fail, connection) {
-         try {
-             connection.disconnect();
-             win('DISCONNECTED');
-             return;
-         } catch (e) {
-             fail(e);
-             return;
-         }
-     };
+    exports.disconnect = function(win, fail, connection) {
+        try {
+            connection.disconnect();
+            win('DISCONNECTED');
+            return;
+        } catch (e) {
+            fail(e);
+            return;
+        }
+    };
 
 
-     require('cordova/windows8/commandProxy').add('ScpHandler', exports);
+    require('cordova/windows8/commandProxy').add('ScpHandler', exports);
 
- });
+});
--- src/windows8/ScpProxy.js
+++ src/windows8/ScpProxy.js
@@ -1,102 +1,102 @@
- cordova.define("com.tlantic.plugins.ssh.ScpProxy", function(require, exports, module) {
+cordova.define("com.tlantic.plugins.ssh.ScpProxy", function(require, exports, module) {
 
-     var hscp = require('com.tlantic.plugins.ssh.ScpHandler');
+    var hscp = require('com.tlantic.plugins.ssh.ScpHandler');
 
-     module.exports = {
-         connect: function(win, fail, args) {
-             'use strict';
+    module.exports = {
+        connect: function(win, fail, args) {
+            'use strict';
 
-             if (args.length !== 4) {
-                 fail('Missing arguments for "connect" action.');
-                 return;
+            if (args.length !== 4) {
+                fail('Missing arguments for "connect" action.');
+                return;
 
-             } else {
+            } else {
 
-                 try {
-                     var host, port, username, password;
+                try {
+                    var host, port, username, password;
 
-                     host = args[0];
-                     port = args[1];
-                     username = args[2];
-                     password = args[3];
+                    host = args[0];
+                    port = args[1];
+                    username = args[2];
+                    password = args[3];
 
-                     hscp.connect(win, fail, host, port, password, username);
-                     return;
+                    hscp.connect(win, fail, host, port, password, username);
+                    return;
 
-                 } catch (e) {
-                     fail(e);
-                     return;
-                 }
-             }
+                } catch (e) {
+                    fail(e);
+                    return;
+                }
+            }
 
-         },
+        },
 
-         disconnect: function(win, fail, args) {
-             'use strict';
+        disconnect: function(win, fail, args) {
+            'use strict';
 
-             if (args.length !== 1) {
-                 fail('Missing arguments for "connect" action.');
-                 return;
+            if (args.length !== 1) {
+                fail('Missing arguments for "connect" action.');
+                return;
 
-             } else {
+            } else {
 
-                 try {
-                     var connectionId, file, path;
+                try {
+                    var connectionId, file, path;
 
-                     connectionId = args[0];
-
-
-                     hscp.disconnect(win, fail, connectionId);
-                     return;
-
-                 } catch (e) {
-                     fail(e);
-                     return;
-                 }
-             }
-         },
-
-         disconnectAll: function(win, fail, args) {
-             'use strict';
-
-             fail('Not implemented');
-         },
-
-         authenticateByKeyboard: function(win, fail, args) {
-             'use strict';
-
-             fail('Not implemented');
-         },
-
-         scp: function(win, fail, args) {
-             'use strict';
-
-             if (args.length !== 3) {
-                 fail('Missing arguments for "connect" action.');
-                 return;
-
-             } else {
-
-                 try {
-                     var connectionId, file, path;
-
-                     connectionId = args[0];
-                     file = args[1];
-                     path = args[2];
+                    connectionId = args[0];
 
 
-                     hscp.scp(win, fail, connectionId, file, path);
-                     return;
+                    hscp.disconnect(win, fail, connectionId);
+                    return;
 
-                 } catch (e) {
-                     fail(e);
-                     return;
-                 }
-             }
-         }
+                } catch (e) {
+                    fail(e);
+                    return;
+                }
+            }
+        },
 
-     };
+        disconnectAll: function(win, fail, args) {
+            'use strict';
 
-     require('cordova/windows8/commandProxy').add('SSH', module.exports);
+            fail('Not implemented');
+        },
 
- });
+        authenticateByKeyboard: function(win, fail, args) {
+            'use strict';
+
+            fail('Not implemented');
+        },
+
+        scp: function(win, fail, args) {
+            'use strict';
+
+            if (args.length !== 3) {
+                fail('Missing arguments for "connect" action.');
+                return;
+
+            } else {
+
+                try {
+                    var connectionId, file, path;
+
+                    connectionId = args[0];
+                    file = args[1];
+                    path = args[2];
+
+
+                    hscp.scp(win, fail, connectionId, file, path);
+                    return;
+
+                } catch (e) {
+                    fail(e);
+                    return;
+                }
+            }
+        }
+
+    };
+
+    require('cordova/windows8/commandProxy').add('SSH', module.exports);
+
+});
